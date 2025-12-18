6a2cd3a6fff77561e80b516e072737fa93ddfbe9
hideki <xzengbu@gmail.com>
2025-12-18 09:40:01 +0900

    Update Analytics.tsx

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -2,23 +2,17 @@
 'use client'
 
 import { useEffect } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { pageview } from '../lib/analytics'
 
-/**
- * App Router用 ページビュー自動追跡コンポーネント
- * layout.tsx の <body> 内に配置
- */
 export function Analytics() {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
 
   useEffect(() => {
     if (pathname) {
-      const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : '')
-      pageview(url)
+      pageview(pathname)
     }
-  }, [pathname, searchParams])
+  }, [pathname])
 
   return null
 }
