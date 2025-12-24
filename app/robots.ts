05e432dbe38248fe75514c92808f61a48f371b72
hideki <xzengbu@gmail.com>
2025-12-24 09:11:47 +0900

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,26 @@
-// app/robots.ts
+// app/robots.ts - SEO完全対応版
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/static/', '/_next/image/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [],
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [],
+      },
+    ],
     sitemap: 'https://portfolio-crystal-dreamscape.vercel.app/sitemap.xml',
+    host: 'https://portfolio-crystal-dreamscape.vercel.app',
   }
 }
